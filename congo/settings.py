cfc8e83dd2eb8187d10ca48322368831133247ed
sanjukhanal <47485803+sanjukhanal@users.noreply.github.com>
2021-10-01 16:20:13 -0400

    Updated postgres config

--- congo/settings.py
+++ congo/settings.py
@@ -13,9 +13,8 @@ env.read_env()
 
 ENV = env.str("FLASK_ENV", default="production")
 DEBUG = ENV == "development"
-SQLALCHEMY_DATABASE_URI = env.str("HEROKU_POSTGRESQL_AQUA_URL")
-if not SQLALCHEMY_DATABASE_URI.startswith("postgresql"):
-    SQLALCHEMY_DATABASE_URI.replace("postgres", "postgresql")
+DATABASE_URL = env.str("DATABASE_URL") or env.str("HEROKU_POSTGRESQL_AQUA_URL")
+SQLALCHEMY_DATABASE_URI = DATABASE_URL
 SECRET_KEY = env.str("SECRET_KEY")
 SEND_FILE_MAX_AGE_DEFAULT = env.int("SEND_FILE_MAX_AGE_DEFAULT")
 BCRYPT_LOG_ROUNDS = env.int("BCRYPT_LOG_ROUNDS", default=13)
@@ -23,7 +22,6 @@ DEBUG_TB_ENABLED = DEBUG
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 CACHE_TYPE = "simple"  # Can be "memcached", "redis", etc.
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 
 NYT_KEY = env.str("NYT_KEY")
 NYT_SECRET = env.str("NYT_SECRET")
