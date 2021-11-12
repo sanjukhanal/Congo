5d1e33e57ebbd82103d22c20a8f16ad0b60655be
sanjukhanal <khanal.sanju@gmail.com>
2021-11-11 22:27:03 -0500

    Fix admin

--- congo/checkout/views.py
+++ congo/checkout/views.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, jsonify, session, render_template, redirect, url_for
+from flask import Blueprint, jsonify, session, render_template, redirect, url_for, request, flash
 from flask_login import login_required, current_user
 
+from .forms import CheckoutForm
 from .models import Checkout
 from ..cart.models import Cart
+from ..utils import flash_errors
 
 blueprint = Blueprint("checkout", __name__, url_prefix="/checkout", static_folder="../static")
 
@@ -35,14 +37,10 @@ def add_book(book_id):
 @blueprint.route("/remove_book/<int:checkout_id>", methods=['GET', 'POST'])
 @login_required
 def remove_book(checkout_id):
-    cart_id = session.get("cart_id")
-    if cart_id is not None:
-        cart = Cart.create(
-            user_id=current_user.id
-        )
-        checkout = Checkout.get_by_id(checkout_id)
-        checkout.delete()
-        return redirect(url_for('checkout.home'))
+    checkout = Checkout.get_by_id(checkout_id)
+    checkout.delete()
+    flash("You deleted the item.", "success")
+    return redirect(url_for('checkout.home'))
 
 
 @blueprint.route("/", methods=['GET', 'POST'])
@@ -62,9 +60,22 @@ def submit():
     cart_id = session.get("cart_id")
     if cart_id:
         cart = Cart.get_by_id(cart_id)
-        cart.update({"is_submitted": True})
-        checkouts = cart.checkouts
-        total = sum([item.book.price for item in checkouts])
+        cart.update(is_submitted=True)
         session.pop("cart_id")
-        return render_template("checkout/thank_you.html", checkouts=checkouts, total=total)
+        return render_template("checkout/thank_you.html")
     return redirect(url_for('user.home'))
+
+
+@blueprint.route("/credit_card", methods=['GET', 'POST'])
+@login_required
+def credit_card():
+    form = CheckoutForm()
+    cart_id = session.get("cart_id")
+    checkouts=None
+    if cart_id:
+        cart = Cart.get_by_id(cart_id)
+        checkouts = cart.checkouts
+    if request.method == "POST" and form.validate_on_submit():
+        return redirect(url_for('checkout.submit'))
+    flash_errors(form)
+    return render_template("checkout/credit_card.html", form=form,  checkouts=checkouts,)
